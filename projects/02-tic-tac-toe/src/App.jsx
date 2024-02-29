0ef5dba3deac1816df9e214f9796681d47bc2ae4
Jaison Quiroga <jeisonrestrepo37@gmail.com>
2024-02-29 14:03:38 -0500

    ajustes en el poryecto

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -6,17 +6,26 @@ import { checkWinnerFrom, checkEndGame } from "./logic/board.js"
 import { WinnerModal } from "./components/WinnerModal.jsx"
 
 function App() {
-  const [board, setBoard] = useState(
-    Array(9).fill(null)
+  const [board, setBoard] = useState( () =>{
+    const boardFromStorage = window.localStorage.getItem('board')
+    if (boardFromStorage) return JSON.parse(boardFromStorage)
+    return Array(9).fill(null)
+  }
   )
 
-  const [turn, setTurn] = useState(TUNRS.X)
+  const [turn, setTurn] = useState(() =>{
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TUNRS.X
+  })
   const [winner, setWinner] = useState(null)
 
   const resetGame = () =>{
     setBoard(Array(9).fill(null))
     setTurn(TUNRS.X)
     setWinner(null)
+
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   }
 
   const updateBoard = (index) => {
@@ -29,6 +38,9 @@ function App() {
     //cambiar el turno
     const newTurn = turn == TUNRS.X ? TUNRS.O : TUNRS.X
     setTurn(newTurn)
+    //guardar partida
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
     //revisar si hay un ganador
     const newWinner = checkWinnerFrom(newBoard)
     if (newWinner){
